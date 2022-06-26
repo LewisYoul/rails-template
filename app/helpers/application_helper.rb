b5ebadb6be3867574feeb3810df341723f7aa818
Lewis Youl <lewis.youl@trustflight.io>
2022-06-26 12:58:45 +0100

    feat: add links to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def navbar_link_to(text, path)
+    classes = if current_page?(path)
+      "border-purple-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium cursor-pointer"
+    else
+      "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+    end
+
+    current_page?(path) ? content_tag(:span, text, class: classes) : link_to(text, path, class: classes)
+  end
 end
