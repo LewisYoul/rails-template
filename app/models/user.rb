2a5167af5e00d5a82a02d92b4ece8a3cd34a465f
Lewis Youl <lewis.youl@trustflight.io>
2022-08-21 21:06:59 +0100

    fix: return correct user activities

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :plan_limited_activities, -> (user) do
     return with_geometry.chronological if user.plan.paid?
 
-    user.activities.where(id: with_geometry.chronological.limit(user.plan.activities_limit).select(:id)).chronological
+    user.activities.where(id: user.activities.with_geometry.chronological.limit(user.plan.activities_limit).select(:id)).chronological
   end, class_name: 'Activity'
 
   accepts_nested_attributes_for :token
