fd966ec1c3557c2a235734efa37fcf940d4806d1
Lewis Youl <lewis.youl@trustflight.io>
2022-11-02 18:59:00 +0000

    feat: use strava orange for landing page

--- app/javascript/react/src/components/Map.jsx
+++ app/javascript/react/src/components/Map.jsx
@@ -142,7 +142,7 @@ function Map() {
     if (isLoading) return null
 
     return (
-      <button onClick={refreshActivities} className="shadow-md absolute mt-4 mr-8 z-500 top-0 right-0 whitespace-nowrap inline-flex items-center justify-center px-2 py-1 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-white hover:bg-gray-100 text-purple-600">
+      <button onClick={refreshActivities} className="shadow-md absolute mt-4 mr-8 z-500 top-0 right-0 whitespace-nowrap inline-flex items-center justify-center px-2 py-1 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-white hover:bg-gray-100 text-purple-600 font-sans">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
           <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
         </svg>
